8bbb040900d5a9b255e01b6102d8249e9c7a0fcd
Wijnter Bunny <michuuuzzz1@gmail.com>
2023-11-08 17:00:37 +0100

    Added a lot of buttons

--- Assets/Scripts/levelManager.cs
+++ Assets/Scripts/levelManager.cs
@@ -6,19 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class levelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadNextScene()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void LoadStartScene()
     {
-        SceneManager.LoadScene("Lose");
+        SceneManager.LoadScene(0);
     }
-    // Update is called once per frame
-    void Update()
+
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
